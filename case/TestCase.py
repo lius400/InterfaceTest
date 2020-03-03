162b15392408e9706b68f10875917f35892de3fb
liuchao <lius400@hotmail.com>
2020-03-03 15:20:09 +0800

    '优化'

--- case/TestCase.py
+++ case/TestCase.py
@@ -29,7 +29,7 @@ class TestCase(unittest.TestCase):
     @data(*datalist)
     def testJDproduct(self,Data):
         #测试用用例描述
-        '''Data["CaseName"]'''
+        print("用例名称：{}".format(Data["CaseName"]))
         self.log.info("Headers:%s"%(Data["Headers"]))
         Result,Response = self.interfacetest.testrequest(Data["URL"],Data["URI"],Data["Param"],Data["RequestForm"],Data["DataForm"],Data["CheckPoint"],Data["Headers"],Data["ID"],Data["CaseName"])
         print("Response:%s"%(Response))
