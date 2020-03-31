b411cc71ff93c203debd530933e8e7209ff81e77
liuchao <lius400@hotmail.com>
2020-03-31 18:25:33 +0800

    优化数据库访问

--- common/DButil.py
+++ common/DButil.py
@@ -108,10 +108,32 @@ class MysqlBase:
 
 if __name__ == '__main__':
     # MysqlBase.testconnect()
-    db = MysqlBase()
+    # db = MysqlBase()
     table_name = "employee"
     data = {'employee_name':'Kathy','`age`':27,'sex':1,'income':8000,'birthday':'2016-08-20'}
-
-    db.clear(table_name)
+    #
+    # db.clear(table_name)
     # db.insert(table_name, data)
     # db.close()
+
+    # create data
+    # datas = {
+    #     'sign_event': [
+    #         {'id': 1, 'name': '红米Pro发布会', '`limit`': 2000, 'status': 1, 'address': '北京会展中心', 'start_time': future_time},
+    #         {'id': 2, 'name': '可参加人数为0', '`limit`': 0, 'status': 1, 'address': '北京会展中心', 'start_time': future_time},
+    #         {'id': 3, 'name': '当前状态为0关闭', '`limit`': 2000, 'status': 0, 'address': '北京会展中心', 'start_time': future_time},
+    #         {'id': 4, 'name': '发布会已结束', '`limit`': 2000, 'status': 1, 'address': '北京会展中心', 'start_time': past_time},
+    #         {'id': 5, 'name': '小米5发布会', '`limit`': 2000, 'status': 1, 'address': '北京国家会议中心', 'start_time': future_time},
+    #     ],
+    #     'sign_guest': [
+    #         {'id': 1, 'realname': 'alen', 'phone': 13511001100, 'email': 'alen@mail.com', 'sign': 0, 'event_id': 1},
+    #         {'id': 2, 'realname': 'has sign', 'phone': 13511001101, 'email': 'sign@mail.com', 'sign': 1, 'event_id': 1},
+    #         {'id': 3, 'realname': 'tom', 'phone': 13511001102, 'email': 'tom@mail.com', 'sign': 0, 'event_id': 5},
+    #     ],
+    # }
+    datas = {
+        table_name: [
+            data,
+        ],
+    }
+    MysqlBase().init_data(datas)
